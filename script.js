8c2170321f3535c35fad6c8e6fa6dd8722a0efa2
anj7726 <anjalibalakrishnan417@gmail.com>
2025-07-05 21:43:25 +0530

    update index.html and script.js to add today's special button

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log("Welcome to Annie's Cakes!!");
 window.onload=function(){
     alert("Welcome to Annie's Cakes!!");
 };
+function showSpecial() {
+    const special = document.getElementById("special");
+    special.style.display = "block";
+}
 setInterval(function(){
 const text=document.getElementById("blinking-text");
 if(text.style.visibility==="hidden"){
